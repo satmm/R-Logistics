53a6f69a30ed2441a99a1d61cb5f726ed8f9830a
Satyam Singh <satyamsingh.is20@sahyadri.edu.in>
2024-08-08 21:20:31 +0530

    Update AdminPage.jsx

--- Frontend/src/Components/AdminPage.jsx
+++ Frontend/src/Components/AdminPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { FaPlus, FaDownload, FaEdit, FaTrash } from 'react-icons/fa';
+import { FaPlus, FaDownload, FaEdit, FaTrash, FaCheckSquare, FaSearch } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './AdminPage.css';
 
 const AdminPage = () => {
@@ -27,6 +29,8 @@ const AdminPage = () => {
   const [selectedEntries, setSelectedEntries] = useState([]);
   const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
   const [deleteId, setDeleteId] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const entriesPerPage = 20;
 
   useEffect(() => {
     fetchEntries();
@@ -41,7 +45,7 @@ const AdminPage = () => {
 
   const fetchEntries = async () => {
     try {
-      const response = await axios.get('https://backend-xi-taupe-70.vercel.app/entries');
+      const response = await axios.get('https://try-theta-lovat.vercel.app/entries');
       const formattedEntries = response.data.map(entry => ({
         ...entry,
         date: new Date(entry.date).toLocaleDateString('en-GB')
@@ -76,10 +80,12 @@ const AdminPage = () => {
     e.preventDefault();
     try {
       if (isEdit) {
-        await axios.put(`https://backend-xi-taupe-70.vercel.app/edit-entry/${formData._id}`, formData);
+        await axios.put(`https://try-theta-lovat.vercel.app/edit-entry/${formData._id}`, formData);
         setIsEdit(false);
+        toast.success('Entry updated successfully');
       } else {
-        await axios.post('https://backend-xi-taupe-70.vercel.app/add-entry', formData);
+        await axios.post('https://try-theta-lovat.vercel.app/add-entry', formData);
+        toast.success('Entry added successfully');
       }
       setShowForm(false);
       fetchEntries();
@@ -97,10 +103,11 @@ const AdminPage = () => {
 
   const handleDelete = async () => {
     try {
-      await axios.delete(`https://backend-xi-taupe-70.vercel.app/delete-entry/${deleteId}`);
+      await axios.delete(`https://try-theta-lovat.vercel.app/delete-entry/${deleteId}`);
       fetchEntries();
       setShowDeleteConfirm(false);
       setDeleteId(null);
+      toast.success('Entry deleted successfully');
     } catch (error) {
       console.error('Error deleting entry:', error);
     }
@@ -130,6 +137,17 @@ const AdminPage = () => {
       setSelectedEntries(selectedEntries.filter((id) => id !== entry._id));
     } else {
       setSelectedEntries([...selectedEntries, entry._id]);
+    }
+  };
+
+  const handleDeleteSelected = async () => {
+    try {
+      await axios.post('https://try-theta-lovat.vercel.app/delete-entries', { ids: selectedEntries });
+      fetchEntries();
+      setSelectedEntries([]);
+      toast.success('Selected entries deleted successfully');
+    } catch (error) {
+      console.error('Error deleting entries:', error);
     }
   };
 
@@ -168,8 +186,40 @@ const AdminPage = () => {
     link.click();
   };
 
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < Math.ceil(entries.length / entriesPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const toggleSelectAll = () => {
+    const currentEntries = entries.slice(startIndex, startIndex + entriesPerPage).map(entry => entry._id);
+    if (selectedEntries.length === currentEntries.length) {
+      setSelectedEntries([]);
+    } else {
+      setSelectedEntries(currentEntries);
+    }
+  };
+
+  const startIndex = (currentPage - 1) * entriesPerPage;
+  const displayedEntries = entries
+    .filter(entry =>
+      entry.driverName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      entry.vehicleNumber.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      entry.date.includes(searchTerm)
+    )
+    .sort((a, b) => new Date(b.date) - new Date(a.date)) // Sort entries by date, latest first
+    .slice(startIndex, startIndex + entriesPerPage);
+
   return (
     <div className="admin-container">
+      <ToastContainer />
       <div className="header-buttons">
         {selectedEntries.length === 0 && (
           <button className="button-33" onClick={() => {
@@ -186,22 +236,23 @@ const AdminPage = () => {
           <FaEdit onClick={() => handleEdit(entries.find(entry => entry._id === selectedEntries[0]))} />
         )}
         {selectedEntries.length > 0 && (
-          <FaTrash onClick={() => {
-            if (selectedEntries.length === 1) {
-              setShowDeleteConfirm(true);
-              setDeleteId(selectedEntries[0]);
-            }
-          }} />
+          <>
+            <FaCheckSquare onClick={toggleSelectAll} />
+            <FaTrash onClick={() => setShowDeleteConfirm(true)} />
+          </>
         )}
       </div>
       <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search by Driver Name, Vehicle Number, or Date"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+        <div className="search-bar-container">
+          <FaSearch className="search-icon" />
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search by Driver Name, Vehicle Number, or Date"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
       </div>
       <div className="table-container">
         <table>
@@ -224,39 +275,41 @@ const AdminPage = () => {
             </tr>
           </thead>
           <tbody>
-            {entries
-              .filter(entry => 
-                entry.driverName.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                entry.vehicleNumber.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                entry.date.includes(searchTerm)
-              )
-              .sort((a, b) => new Date(b.date) - new Date(a.date)) // Sort entries by date, latest first
-              .map(entry => (
-                <tr key={entry._id}>
-                  <td>
-                    <input
-                      type="checkbox"
-                      checked={selectedEntries.includes(entry._id)}
-                      onChange={() => handleCheckboxChange(entry)}
-                    />
-                  </td>
-                  <td>{entry.driverName}</td>
-                  <td>{entry.vehicleNumber}</td>
-                  <td>{entry.date}</td>
-                  <td>{entry.present ? 'Yes' : 'No'}</td>
-                  <td>{entry.advance}</td>
-                  <td>{entry.cngCost}</td>
-                  <td>{entry.driverSalary}</td>
-                  <td>{entry.shiftTo}</td>
-                  <td>{entry.billTo}</td>
-                  <td>{entry.partyRate}</td>
-                  <td>{entry.gstPercent}</td>
-                  <td>{entry.vehicleRate}</td>
-                  <td>{entry.remark}</td>
-                </tr>
-              ))}
+            {displayedEntries.map((entry) => (
+              <tr key={entry._id}>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={selectedEntries.includes(entry._id)}
+                    onChange={() => handleCheckboxChange(entry)}
+                  />
+                </td>
+                <td>{entry.driverName}</td>
+                <td>{entry.vehicleNumber}</td>
+                <td>{entry.date}</td>
+                <td>{entry.present ? 'Yes' : 'No'}</td>
+                <td>{entry.advance}</td>
+                <td>{entry.cngCost}</td>
+                <td>{entry.driverSalary}</td>
+                <td>{entry.shiftTo}</td>
+                <td>{entry.billTo}</td>
+                <td>{entry.partyRate}</td>
+                <td>{entry.gstPercent}</td>
+                <td>{entry.vehicleRate}</td>
+                <td>{entry.remark}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
+      </div>
+      <div className="pagination-controls">
+        <button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Prev
+        </button>
+        <span>{currentPage}</span>
+        <button onClick={handleNextPage} disabled={currentPage === Math.ceil(entries.length / entriesPerPage)}>
+          Next
+        </button>
       </div>
       {showForm && (
         <div className="popup-form">
@@ -264,137 +317,70 @@ const AdminPage = () => {
           <form onSubmit={handleSubmit}>
             <label>
               Driver Name:
-              <input
-                type="text"
-                name="driverName"
-                value={formData.driverName}
-                onChange={handleChange}
-                required
-              />
+              <input type="text" name="driverName" value={formData.driverName} onChange={handleChange} required />
             </label>
             <label>
               Vehicle Number:
-              <input
-                type="text"
-                name="vehicleNumber"
-                value={formData.vehicleNumber}
-                onChange={handleChange}
-                required
-              />
+              <input type="text" name="vehicleNumber" value={formData.vehicleNumber} onChange={handleChange} required />
             </label>
             <label>
               Date:
-              <input
-                type="date"
-                name="date"
-                value={formData.date}
-                onChange={handleChange}
-                required
-              />
+              <input type="date" name="date" value={formData.date} onChange={handleChange} required />
             </label>
             <label>
-              Present:
-              <input
-                type="checkbox"
-                name="present"
-                checked={formData.present}
-                onChange={handleChange}
-              />
+              Status:
+              <select name="present" value={formData.present} onChange={handleChange}>
+                <option value="present">Present</option>
+                <option value="absent">Absent</option>
+              </select>
             </label>
+
             <label>
               Advance:
-              <input
-                type="number"
-                name="advance"
-                value={formData.advance}
-                onChange={handleChange}
-              />
+              <input type="number" name="advance" value={formData.advance} onChange={handleChange} required />
             </label>
             <label>
               CNG Cost:
-              <input
-                type="number"
-                name="cngCost"
-                value={formData.cngCost}
-                onChange={handleChange}
-              />
+              <input type="number" name="cngCost" value={formData.cngCost} onChange={handleChange} required />
             </label>
             <label>
               Driver Salary:
-              <input
-                type="number"
-                name="driverSalary"
-                value={formData.driverSalary}
-                onChange={handleChange}
-              />
+              <input type="number" name="driverSalary" value={formData.driverSalary} onChange={handleChange} required />
             </label>
             <label>
               Shift To:
-              <input
-                type="text"
-                name="shiftTo"
-                value={formData.shiftTo}
-                onChange={handleChange}
-              />
+              <input type="text" name="shiftTo" value={formData.shiftTo} onChange={handleChange} />
             </label>
             <label>
               Bill To:
-              <input
-                type="text"
-                name="billTo"
-                value={formData.billTo}
-                onChange={handleChange}
-              />
+              <input type="text" name="billTo" value={formData.billTo} onChange={handleChange} />
             </label>
             <label>
               Party Rate:
-              <input
-                type="number"
-                name="partyRate"
-                value={formData.partyRate}
-                onChange={handleChange}
-              />
+              <input type="number" name="partyRate" value={formData.partyRate} onChange={handleChange} required />
             </label>
             <label>
               GST %:
-              <input
-                type="number"
-                name="gstPercent"
-                value={formData.gstPercent}
-                onChange={handleChange}
-              />
+              <input type="number" name="gstPercent" value={formData.gstPercent} onChange={handleChange} required />
             </label>
             <label>
               Vehicle Rate:
-              <input
-                type="number"
-                name="vehicleRate"
-                value={formData.vehicleRate}
-                onChange={handleChange}
-              />
+              <input type="number" name="vehicleRate" value={formData.vehicleRate} onChange={handleChange} required />
             </label>
             <label>
               Remark:
-              <input
-                type="text"
-                name="remark"
-                value={formData.remark}
-                onChange={handleChange}
-              />
+              <input type="text" name="remark" value={formData.remark} onChange={handleChange} />
             </label>
-            <button type="submit">{isEdit ? 'Save Changes' : 'Add Entry'}</button>
+            <button type="submit">{isEdit ? 'Update Entry' : 'Add Entry'}</button>
             <button type="button" onClick={() => setShowForm(false)}>Cancel</button>
           </form>
         </div>
       )}
       {showDeleteConfirm && (
-        <div className="delete-confirm-popup">
-          <p>Are you sure you want to delete this entry?</p>
-          <button className="button-33" onClick={handleDelete}>Yes</button>
-          <button className="button-33" onClick={() => {
-            setShowDeleteConfirm(false);
-            setDeleteId(null);
-          }}>No</button>
+        <div className="popup-confirm">
+          <p>Are you sure you want to delete the selected entry(ies)?</p>
+          <button className="yes-button" onClick={handleDeleteSelected}>Yes</button>
+          <button className="no-button" onClick={() => setShowDeleteConfirm(false)}>No</button>
         </div>
       )}
     </div>
